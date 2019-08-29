72feffe22876fad1d114375cc8ff41b40f0d1c3c
Eric-Andrade <e.torres@itecor.ch>
2019-08-29 11:54:11 -0500

    Antd + Typescript (custom server)

---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+import {
+  Form,
+  InputNumber,
+  DatePicker,
+  Slider,
+  Button
+} from "antd";
+
+const FormItem = Form.Item;
+
+export default () => (
+  <div style={{ marginTop: 100 }}>
+    <Form layout="horizontal">
+      <FormItem
+        label="Input Number"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <InputNumber
+          size="large"
+          min={1}
+          max={10}
+          style={{ width: 100 }}
+          defaultValue={3}
+          name="inputNumber"
+        />
+        <a href="#">Link</a>
+      </FormItem>
+
+      <FormItem label="Slider" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+        <Slider defaultValue={70} />
+      </FormItem>
+
+      <FormItem
+        label="DatePicker"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <DatePicker name="startDate" />
+      </FormItem>
+      <FormItem style={{ marginTop: 48 }} wrapperCol={{ span: 8, offset: 8 }}>
+        <Button size="large" type="primary" htmlType="submit">
+          OK
+        </Button>
+        <Button size="large" style={{ marginLeft: 8 }}>
+          Cancel
+        </Button>
+      </FormItem>
+    </Form>
+  </div>
+);
